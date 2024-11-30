a92dcdfac53ed9730ff12201f0ce933935111777
Ryan Punt <ryan@mirum.org>
2024-11-30 15:41:34 -0600

    pass the default config to core's config method

--- jiracli/__init__.py
+++ jiracli/__init__.py
@@ -1,5 +1,7 @@
 import cac_core as cac
 from importlib import metadata
+import os
+import yaml
 
 try:
     __version__ = metadata.version(__package__)
@@ -12,7 +14,13 @@ import keyring
 log = cac.logger.new(__name__)
 log.debug(f"Initializing {__name__} version {__version__}")
 
-config = cac.config.load(__name__)
+default_config = {}
+default_config_dir = os.path.join(os.path.dirname(__file__), 'config')
+default_config_file = os.path.join(default_config_dir, f"{__name__}.yaml")
+if os.path.exists(default_config_file):
+    with open(default_config_file, 'r') as f:
+        default_config.update(yaml.safe_load(f))
+config = cac.config.load(__name__, default_config)
 
 log.info(f"user config path: {config['config_file_path']}")
 
