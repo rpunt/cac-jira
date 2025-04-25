14309270beca2b489f29d7cdf0ac199e044308eb
Ryan Punt <ryan.punt@ext.doordash.com>
2025-04-25 15:49:31 -0500

    add a note

--- cac_jira/commands/issue/list.py
+++ cac_jira/commands/issue/list.py
@@ -80,5 +80,7 @@ class IssueList(JiraIssueCommand):
             )
             models.append(model)
 
+        # TODO: test this also
+        # printer = cac.output.Output(args)
         printer = cac.output.Output({"output": "table"})
         printer.print_models(models)
